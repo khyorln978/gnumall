6aa4a12df63812851ec6fff67043b3673e188523
Hyeong Suk <hyngsk.o@gmail.com>
2020-10-19 10:14:02 +0900

    Revert "chore :메인페이지, 의존성추가"
    This reverts commit c5da2bb8fa609a66d15d7bff4de3249e9ec584d8.

--- gnumall/src/main/java/gnumall/mainpage/MainController.java
+++ gnumall/src/main/java/gnumall/mainpage/MainController.java
@@ -1,26 +1,16 @@
 package gnumall.mainpage;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class MainController {
-	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
-
+	
 	@RequestMapping(value = "/", method = {RequestMethod.GET})
-	public String getMainPage(HttpServletRequest request, HttpServletResponse response) {
-		logger.info("Hello index page");
-		logger.info("request : "+ request + "response : "+ response);
+	public String getMainPage() {
 		
-		
-		return "/index";
+		return "index";
 	}
 	
 }
