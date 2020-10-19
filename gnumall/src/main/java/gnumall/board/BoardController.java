5fe423ddbd2947964a560a7130ae5eeca475fbf2
Hyeong Suk <hyngsk.o@gmail.com>
2020-10-19 19:52:10 +0900

    feature : 부트스트랩, 로그인, 게시판, 상품 페이지 추가 fix : 새로운 파일 git 스테이징 오류 수정
    - .gitignore 수정
     http://www.gitignore.io 를 통해
     eclipse intellij+iml java-web 추가,
     기존에 /gnumall/ 구문을 없애줌으로써
     새로운 파일 인식가능하게함
    
    -Thymeleaf 템플릿 엔진추가
     (servlet-context.xml, pom.xml)수정,
     레이아웃 구조 추가
    
    -bootstrap 추가
     lux theme by Bootswatch.
     참고 : https://bootswatch.com/lux/

---
+++ gnumall/src/main/java/gnumall/board/BoardController.java
@@ -0,0 +1,26 @@
+package gnumall.board;
+
+import gnumall.mainpage.MainController;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class BoardController {
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	@RequestMapping(value = "/board", method = {RequestMethod.GET})
+	public String getPostTable(HttpServletRequest request, HttpServletResponse response) {
+		logger.info("Hello index page");
+		logger.info("request : "+ request + "response : "+ response);
+
+
+		return "/content/board";
+	}
+
+}
---
+++ gnumall/src/main/java/gnumall/login/LoginController.java
@@ -0,0 +1,25 @@
+package gnumall.login;
+
+import gnumall.mainpage.MainController;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LoginController {
+	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	@RequestMapping(value = "/login", method = {RequestMethod.GET})
+	public String getLoginPage(HttpServletRequest request, HttpServletResponse response) {
+		logger.info("Hello index page");
+		logger.info("request : "+ request + "response : "+ response);
+
+
+		return "/content/login";
+	}
+}
